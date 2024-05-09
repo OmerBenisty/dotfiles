d6997c25d848297a28a567cfc550f55eec2d0b69
Omer Benisty <omerb@inmanage.co.il>
2024-05-09 16:43:25 +0300

    Removed Code

--- Code/User/History/-3f0e4721/wuZ0.sh
+++
@@ -1,5 +0,0 @@
-curl --request POST \
---url https://api.sendgrid.com/v3/mail/send \
---header 'Authorization: Bearer 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' \
---header 'Content-Type: application/json' \
---data '{"personalizations":[{"to":[{"email":"omerb@inmanage.co.il","name":"John Doe"}],"subject":"Hello, World!"}],"content": [{"type": "text/plain", "value": "Heya!"}],"from":{"email":"test@grow.security","name":"Sam Smith"},"reply_to":{"email":"test@grow.security","name":"Sam Smith"}}'
